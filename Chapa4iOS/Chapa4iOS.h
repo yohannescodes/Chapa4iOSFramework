d017d586f4f750e75facd2a309a1012688350f4c
Yohannes Haile <yohannes@kemercode.com>
2024-05-04 06:47:13 +0300

    Initial Commit

---
+++ Chapa4iOS/Chapa4iOS.h
@@ -0,0 +1,18 @@
+//
+//  Chapa4iOS.h
+//  Chapa4iOS
+//
+//  Created by Yohannes Haile on 5/4/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Chapa4iOS.
+FOUNDATION_EXPORT double Chapa4iOSVersionNumber;
+
+//! Project version string for Chapa4iOS.
+FOUNDATION_EXPORT const unsigned char Chapa4iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chapa4iOS/PublicHeader.h>
+
+
